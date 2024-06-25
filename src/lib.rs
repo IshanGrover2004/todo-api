c9075747f9548552ffe3f82c7e8258c6f2c0bec6
IshanGrover2004 <groverishan2004@gmail.com>
2024-06-25 19:58:51 +0530

    feat: Add struct & enums for User, Todo, Status info

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,34 @@
+use std::{collections::HashMap, sync::Arc};
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use tokio::sync::Mutex;
+use uuid::Uuid;
+
 pub mod error;
 pub mod handler;
 
+pub type AppState = Arc<Mutex<HashMap<Uuid, User>>>;
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct User {
+    id: Uuid,
+    tasks: HashMap<u32, Task>,
+    last_task_id: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Task {
+    id: Option<u32>,
+    title: String,
+    description: String,
+    due_date: DateTime<Utc>,
+    status: TaskStatus,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub enum TaskStatus {
+    Todo,
+    InProgress,
+    Done,
+}
